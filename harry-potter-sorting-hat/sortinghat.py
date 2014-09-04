ef7b5c40d16ff411d494095285894b579d5f17ae
Ian Rose <ianrose14@gmail.com>
2014-09-03 20:24:35 -0400

    add sortinghat.py for teh funz

---
+++ harry-potter-sorting-hat/sortinghat.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2.7
+
+import curses
+import random
+import subprocess
+import time
+
+
+# globals
+houses = [None, 'hufflepuff.mp3', 'ravenclaw.mp3', 'slytherin.mp3']
+
+
+def next_file():
+  queue = []
+
+  while True:
+    if len(queue) == 0:
+      indexes = range(len(houses))
+      random.shuffle(indexes)
+      for index in indexes:
+        if index == 0:
+          # special case for gryffindor because we have 2 options; pick one randomly
+          filename = random.choice(['gryffindor1.mp3', 'gryffindor2.mp3'])
+        else:
+          filename = houses[index]
+        queue.append(filename)
+
+    print 'next up:', queue[0]
+    yield queue.pop(0)
+
+
+def play_mp3(filename):
+  subprocess.call(['afplay', filename])
+
+
+def run(stdscr):
+  stdscr.keypad(True)
+  curses.cbreak()
+
+  for filename in next_file():
+    stdscr.getch()
+    play_mp3(filename)
+
+
+#
+# MAIN
+#
+def main():
+  random.seed(time.time())
+  curses.wrapper(run)
+
+
+if __name__ == '__main__':
+  main()
