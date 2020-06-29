24839a93e2b333ebbf332e0ca471b8eeb6dacc49
Ian Rose <ianrose14@gmail.com>
2020-06-29 11:00:43 -0400

    wrap it

--- exception.js
+++ exception.js
@@ -1,8 +1,16 @@
+function blowup() {
+	x = 4;
+	x.foo();
+}
+
+function wrapper() {
+	blowup();
+}
+
 console.log('before');
 setTimeout(() => {
 	console.log('firing');
-	x = 4;
-	x.foo();
+	wrapper();
 	console.log('done firing');
 }, 3000);
 console.log('after');
